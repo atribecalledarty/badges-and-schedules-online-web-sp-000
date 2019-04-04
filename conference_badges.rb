ab1fa084ccdc6fc2bbe8246663ebb797f934ce10
Arthur Song <arth3rs0ng@gmail.com>
2019-04-04 00:58:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(array)
+  batch_array = []
+  array.each do |name|
+    batch_array << badge_maker(name)
+  end
+  
+end
